cfda11b8866c30fb5c9ec30fd55207f607c4af57
Intissar <intissar.elmezrouai@gmail.com>
2020-09-16 11:14:50 +0400

    test2

---
+++ invoices.js
@@ -0,0 +1,58 @@
+(function () {
+  var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function (schemaCallback) {
+    var cols = [ {
+      id: "invoiceNum",
+      alias: "The Invoice Number",
+      dataType: tableau.dataTypeEnum.string
+  }, {
+      id: "total",
+      alias: "The Invoice Total",
+      dataType: tableau.dataTypeEnum.int
+  }];
+
+  var tableSchema = {
+      id: "Invoices",
+      alias: "List of Invoices",
+      columns: cols
+  };
+
+  schemaCallback([tableSchema]);
+};
+
+myConnector.getData = function(table, doneCallback) {
+  $.getJSON("http://192.168.1.51:80/rest/INVOICES", function(resp) {
+    
+      var result = resp.__ENTITIES,
+      
+          tableData = [];
+          console.log(result)
+      // Iterate over the JSON object
+      
+      for (var i = 0, len = result.length; i < len; i++) {
+      
+          tableData.push({
+              "invoiceNum": result[i].Invoice_Number,
+              "total": result[i].Total
+          });
+      }
+
+      table.appendRows(tableData);
+      doneCallback();
+      setTimeout(function(){tableau.dataCallback(result, "", false)}, 3000);
+  });
+};
+
+  tableau.registerConnector(myConnector);
+  $(document).ready(function () {
+    setTimeout(function(){tableau.submit()}, 3000);
+  });
+  $(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "Invoices";
+        tableau.submit();
+       
+    });
+});
+})();
