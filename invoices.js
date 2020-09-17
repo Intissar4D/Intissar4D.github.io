e0604fa285d5b671831c3ec688c79bf3bffe5c1a
Intissar <intissar.elmezrouai@gmail.com>
2020-09-17 13:15:55 +0400

    add https port

--- invoices.js
+++ invoices.js
@@ -22,7 +22,7 @@
 };
 
 myConnector.getData = function(table, doneCallback) {
-  $.getJSON("http://192.168.1.51:80/rest/INVOICES", function(resp) {
+  $.getJSON("https://192.168.1.51:443/rest/INVOICES", function(resp) {
     
       var result = resp.__ENTITIES,
       
